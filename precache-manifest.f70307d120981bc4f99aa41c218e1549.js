f6cadc934e42ffad5b55fc37d2aa9f1056875f24
luke01455 <luke0145555@hotmail.co.uk>
2019-08-28 18:19:44 +0100

    Updates

---
+++ precache-manifest.f70307d120981bc4f99aa41c218e1549.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9f4dce54a91f8bcd04ae433871b020d",
+    "url": "/portfolioLJ/index.html"
+  },
+  {
+    "revision": "092a2dc35e8f87716469",
+    "url": "/portfolioLJ/static/css/main.21eb18a4.chunk.css"
+  },
+  {
+    "revision": "934a98e24660496481f3",
+    "url": "/portfolioLJ/static/js/2.1f104ba8.chunk.js"
+  },
+  {
+    "revision": "092a2dc35e8f87716469",
+    "url": "/portfolioLJ/static/js/main.7400e2c8.chunk.js"
+  },
+  {
+    "revision": "92b228fb7410fbf39154",
+    "url": "/portfolioLJ/static/js/runtime~main.48c59e9f.js"
+  }
+]);
