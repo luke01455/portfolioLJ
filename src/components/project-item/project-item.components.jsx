2729769bb66e57210ec57405e250b9f8df272574
luke01455 <luke0145555@hotmail.co.uk>
2019-08-30 20:11:30 +0100

    pretty much finished for now. made gifs much cleared, added more responsiveness, added icons for github and deployed site viewing

--- src/components/project-item/project-item.components.jsx
+++ src/components/project-item/project-item.components.jsx
@@ -1,26 +1,41 @@
 import React from 'react';
 
+import githubl from '../../assets/images/githubl.png';
+import searchl from '../../assets/images/searchl.png';
+
 import './project-item.styles.scss';
 
-const ProjectItem = ({title, description, imageUrl}) => (
+const ProjectItem = ({ title, description, imageUrl, gitHub, deploy }) => (
     <div className='project-item'>
         <div className='project-wrapper'>
+
             <div className='image-wrapper'>
                 <div className='background-image'
-                    style={{
-                        backgroundImage: `url(${imageUrl})`
-                    }}>
+                    // style={{
+                    //     backgroundImage: `url(${imageUrl})`
+                    // }}
+                    dangerouslySetInnerHTML={{__html:imageUrl}}
+                    > 
                 </div>
             </div>
             <div className='desc-text'>
-                <div> {title.toUpperCase()}
+                <div className='title'> {title.toUpperCase()}</div>
+                <div className='description'>{description}</div>
+                <div className='git-deploy-wrapper'>
+                    <a href={gitHub} target="_blank" rel="noopener noreferrer">
+                        <div className='github'>
+                            <img src={githubl} className="github-img" alt="menu-button" />
+                        </div>
+                    </a>
+                    <a href={deploy} target="_blank" rel="noopener noreferrer">
+                        <div className='deploy'>
+                            <img src={searchl} className="deploy-img" alt="menu-button" />
+                        </div>
+                    </a>
                 </div>
-
-                <div>{description}</div>
-
             </div>
         </div>
-    </div>
+</div>
 )
 
 export default ProjectItem;
--- src/pages/projectspage/projects-page.component.jsx
+++ src/pages/projectspage/projects-page.component.jsx
@@ -11,32 +11,40 @@ class ProjectsPage extends React.Component {
         sections: [
         {
             title: 'Crwn-Clothing',
-            imageUrl: 'https://media.giphy.com/media/kafnIzCJ12DsEMu5W1/giphy.gif',
+            imageUrl: '<iframe src="https://giphy.com/embed/kafnIzCJ12DsEMu5W1" height=100% frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
             id: 1,
             linkUrl: '',
-            description: 'Crwn Clothing is an e-commerce site, made in react, using firebase for google login'
+            description: 'Crwn Clothing is an e-commerce site, made in react, using firebase for google login',
+            gitHub: 'https://github.com/luke01455/crwn-clothing',
+            deploy: 'https://luke01455.github.io/crwn-clothing/#/'
           },
           {
             title: 'Repeat-Mobile',
-            imageUrl: 'https://media.giphy.com/media/gdfcoij5piIdVFV4mJ/giphy.gif',
+            imageUrl: '<iframe src="https://giphy.com/embed/gdfcoij5piIdVFV4mJ" height=100% frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
             id: 2,
             linkUrl: '',
-            description: 'This is going to be the mobile front-end version for our android repeat application which records sound on a loop'
+            description: 'This is going to be the mobile front-end version for our android repeat application which records sound on a loop',
+            gitHub: 'https://github.com/luke01455/repeat-mobile2',
+            deploy: 'https://luke01455.github.io/repeat-mobile2/#/'
 
           },
           {
             title: 'Forkify',
-            imageUrl: 'https://media.giphy.com/media/iesTI3bXamSEAhRuRr/giphy.gif',
+            imageUrl: '<iframe src="https://giphy.com/embed/iesTI3bXamSEAhRuRr" height=100% frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
             id: 3,
             linkUrl: '',
-            description: 'Made following a Udemy tutorial, Forkify searches an API for recipes, displays the ingredients, lets you add a meal to your favourites, and puts the ingredients in a shopping cart'
+            description: 'Made following a Udemy tutorial, Forkify searches an API for recipes, displays the ingredients, lets you add a meal to your favourites, and puts the ingredients in a shopping cart',
+            gitHub: 'https://github.com/luke01455/forkify',
+            deploy: 'https://luke01455.github.io/forkify/'
           },
           {
             title: 'Budgety',
-            imageUrl: 'https://media.giphy.com/media/dyA3vxeewhadJQxDsK/giphy.gif',
+            imageUrl: '<iframe src="https://giphy.com/embed/dyA3vxeewhadJQxDsK" height=100% frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
             id: 4,
             linkUrl: '',
-            description: 'Made following a Udemy tutorial, budgety lets you plan out this months budget, by letting you add expenses, and add incomes, you can also remove anything youve added'
+            description: 'Made following a Udemy tutorial, budgety lets you plan out this months budget, by letting you add expenses and incomes, you can also remove anything youve added',
+            gitHub: 'https://github.com/luke01455/budgety',
+            deploy: ''
           }
         ]
        }
